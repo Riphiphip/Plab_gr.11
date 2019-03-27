0ea971a2723700430d80aab41b14f602916b482a
riphiphip <halvor.bjorstad@gmail.com>
2019-03-27 19:07:34 +0100

    Fungerende app

--- PCApp/ZumoApp/src/mainApp/ZumoController.java
+++ PCApp/ZumoApp/src/mainApp/ZumoController.java
@@ -1,13 +1,57 @@
 package mainApp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ZumoController {
+	PLabSerial btSerial;
+	
+	@FXML
+	ListView<String> comList;
+	@FXML
+	TextField cmd;
+	@FXML
+	TextArea terminal;
 
 	@FXML
 	public void initialize() {
-
+		btSerial = new PLabSerial();
+		for (String s :btSerial.getPortNames()) {
+			comList.getItems().add(s);
+		}
 	}
-
+	
+	
+	public void selectPort(String portName, PLabSerial serial) {
+		serial.closePort();
+		serial.openPort(portName);
+	}
+	
+	@FXML
+	public void handleComListClick() {
+		selectPort(comList.getSelectionModel().getSelectedItem(),btSerial);
+		System.out.println("Connected to " + btSerial.getOpenPortName());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+		}
+	}
+	
+	@FXML
+	public void handleSendClick() {
+		btSerial.sendMessage(cmd.getText());
+		if (terminal.getText().equals("")) {
+			terminal.setText(cmd.getText());
+		}else {
+			terminal.setText(terminal.getText()+"\n"+cmd.getText());
+			cmd.setText("");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
